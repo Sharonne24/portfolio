3e7b429e7fef736e621896cb472d96418ab09e19
Sharonne24 <sawuor395@gmail.com>
2024-05-23 12:32:41 +0300

    navbar socials

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,32 +1,6 @@
-// import React from 'react';
-// import './Styles.css'; // Import the CSS file for styling
-
-// const Navbar = () => {
-//   return (
-//     <header className="navbar">
-//       <div className="navbar-container">
-//         <div className="logo">
-//           <a href="#hero">SA.</a> {/* Replace "JH." with your initials or logo */}
-//         </div>
-//         <nav>
-//           <ul className="nav-links">
-//             <li><a href="#hero">Home</a></li>
-//             <li><a href="#about">About</a></li>
-//             <li><a href="#skills">Skills</a></li>
-//             <li><a href="#portfolio">Portfolio</a></li>
-//             <li><a href="#contact">Contact</a></li>
-//           </ul>
-//         </nav>
-//       </div>
-//     </header>
-//   );
-// };
-
-// export default Navbar;
-
 
 import React from 'react';
-import { FaLinkedin, FaGithub, FaTwitter } from 'react-icons/fa';
+import { FaLinkedin, FaGithub, FaInstagram } from 'react-icons/fa';
 import './Styles.css';
 
 const Navbar = () => {
@@ -42,9 +16,9 @@ const Navbar = () => {
         </ul>
       </nav>
       <div className="nav-socials">
-        <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
-        <a href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
-        <a href="https://twitter.com/yourprofile" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
+        <a href="https://www.linkedin.com/in/sharon-awuor-313b582a2/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+        <a href="https://github.com/Sharonne24" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+        <a href="https://www.instagram.com/nerdybaine/" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
       </div>
     </header>
   );
